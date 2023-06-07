d2db0e3ddd8ac5d415863994a14bd0ea76bb6a1a
Tze-Ying <tzeying.c@gmail.com>
2023-06-07 10:25:56 +0800

    added some main branch code because of the merge conflict

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import AvatarWelcomeModal from './components/modals/AvatarWelcomeModal';
 import Form from './components/modals/Form'
 
+// ernest's part, which is not needed by char and I's ui
+// import MainLayout from './components/MainLayout'
+// import WelcomeScreen from './components/modals/WelcomeScreen';
+// import AccSelectionScreen from './components/modals/AccSelectionScreen.js';
+// import CreateAvatarResponse from './components/api-calls-avatar/CreateAvatarResponse';
+// import CreateInput from './components/api-calls-avatar/CreateInput';
+
 function App() {
 
   const [redirectActive, setRedirectActive] = useState(1);
@@ -14,6 +21,21 @@ function App() {
     <div className='main'>
       {redirectActive === 1 && <AvatarWelcomeModal setRedirectActive={setRedirectActive} />}
       {redirectActive === 2 && <Form />}
+
+      {/* ernest's part, which is not needed by char and I's ui */}
+      {/* <button onClick={() => openModal('welcome')}>Open Welcome Screen</button>
+      <button onClick={() => openModal('accSelection')}>Open AccSelection Screen</button>
+      {/* Below sets rule for what is rendered by specifying the activePage */}
+      {/* {activePage === 'welcome' && (
+        //  Below passes closeModal funcyion as a prop so that the WelcomeScreen component can use it.
+        <WelcomeScreen closeModal={closeModal} />
+      )}
+      {activePage === 'accSelection' && (
+        <AccSelectionScreen closeModal={closeModal} />
+      )}
+      <MainLayout />
+      <CreateAvatarResponse />
+      <CreateInput /> */}
     </div>
   );
 }
